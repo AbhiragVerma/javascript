43484d8516846dccabcd77db152736bdc534db46
Abhirag Verma <abhiragverma1@gmail.com>
2025-06-10 21:25:03 +0530

    new learning

--- arrows.js
+++ arrows.js
@@ -24,3 +24,13 @@ const chai =  () => {
 }
 
 chai()
+
+const addone = (num1, num2) => (num1 + num2)  //this is shortcut way of writing functions in form of arrow functions and without return or bata dete hai ki curly brackets me return likha jata hai naki parenthesis me 
+
+// console.log(addone(5, 7))
+
+const addTwo = (num1, num2) => {username: "Abhirag Verma"} // it gives the undefined because we have used curly brackets and with out return 
+console.log(addTwo(5, 8));
+
+const addthree = (num1, num2) => ({username: "Abhirag Verma"})
+console.log(addthree(8, 9))
