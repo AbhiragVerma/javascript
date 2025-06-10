e4d40d60c04c5017522affd00c72f62674aef28e
Abhirag Verma <abhiragverma1@gmail.com>
2025-06-10 20:47:26 +0530

    new arrow

--- arrows.js
+++ arrows.js
@@ -1,1 +1,12 @@
-console.log("Abhirag verma");
+const user = {
+    username: "Abhirag Verma",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, Welcome to the website.`);
+        console.log(this);
+    }
+}
+user.welcomeMessage()
+user.username = "Aviral"
+user.welcomeMessage()
