838854a9086260efd5886cb4ba0e745bc1623122
Abhirag Verma <abhiragverma1@gmail.com>
2025-06-12 04:57:33 +0530

    new loops learing

---
+++ iteration/two.js
@@ -0,0 +1,29 @@
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index +2
+}
+
+let myArray = ['Flash', 'Superman', 'Batman']
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+// do {
+    
+// } while (condition);     this is syntax for do while loop 
+
+// let score = 0
+// do {
+//     console.log(`your score is ${score}`);
+//     score ++
+// } while (score <= 10);
+
+let score = 11
+do {
+    console.log(`your score is ${score}`);
+    score ++
+} while (score <= 10);
