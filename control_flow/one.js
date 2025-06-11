078574037e63581b6bdb97bf8ec5402c905ead19
Abhirag Verma <abhiragverma1@gmail.com>
2025-06-11 16:02:29 +0530

    new things

--- control_flow/one.js
+++ control_flow/one.js
@@ -18,4 +18,25 @@ if (score > 100) {
     console.log(`User power: ${power}`);
 }
 
-console.log(`User power: ${power}`);
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"); this is known as implicit scope yaani ki maan liya hai ki woh scope me hai or yeh one liner code hota hai jo ";" isse khatam hota hai
+
+// if (balance < 500){
+//     console.log("less than");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true 
+const debitCard = true 
+
+if (userLoggedIn && debitCard) {
+    console.log("allow to buy courses");
+} else {
+    console.log("you are not eligible to buy the courses");
+}
