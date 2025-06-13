2ed7267b8e203ac76b8dbe307a43d3131626c480
Abhirag Verma <abhiragverma1@gmail.com>
2025-06-13 20:20:19 +0530

    new changes

---
+++ iteration/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// array ki bhi keys hoti hai jo ki numbers hoti hai or start zero se hoti hai and objects ka invention he iss liye hua tha kyunki objects hum log jo marzi ho voh keys daal sakte hai or arrays mei by default numbers he set hoti hai
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
