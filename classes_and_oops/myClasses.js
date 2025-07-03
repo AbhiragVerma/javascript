ae006e76a246b8e38703091e983923a327a035fd
Abhirag Verma <abhiragverma1@gmail.com>
2025-07-03 15:59:58 +0530

    knowing of class and behind the scenes of that and this ka use

---
+++ classes_and_oops/myClasses.js
@@ -0,0 +1,64 @@
+// ES 6 
+
+class User {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+
+
+}
+
+const player = new User('abhirag verma', 'abhi@google.com', '123')
+console.log(player.encryptPassword());
+
+
+// sir ka code hai yeh
+
+
+class User {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+
+}
+
+const chai = new User("chai", "chai@gmail.com", "123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
